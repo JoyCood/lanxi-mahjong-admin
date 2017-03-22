3101b4ae8d2ef4e4f9aeb370c470b0923d3938fd
JoyCood <joy.cood@gmail.com>
2017-03-22 16:51:00 +0800

    项目初始化

---
+++ conf/config.php
@@ -0,0 +1,31 @@
+<?php !defined('TANG_FENG') AND exit('Access Denied!');
+
+define('AdminTemplatePath', DOC_ROOT . '/view');
+
+class Config
+{
+    public static $session = array(
+        'REGION_USER' => 'lanxi_username',
+	'REGION_GROUP' => 'lanxi_group',
+    );
+    
+    public static $admin = array(
+        'title' => '网站标题',
+	'listRowsNum' => 30,
+	
+	'slim' => array(
+	    'templates.path' => AdminTemplatePath
+        ),
+	'sessionSecret' => '^_^ admin ^_^',
+	'resVersion' => 1,
+	'res3rdVersion' => 1
+    );
+
+    //数据库表
+    public static $db = array(
+	'DB' => 'lanxi',
+	'collection' => array(
+	    'admin'
+	) 
+    );
+}
