b7358f8d73daf0417d066f596fddf6626c8c9c32
joy <joy.cood@gmail.com>
2017-04-27 16:15:21 +0800

    加密、解密参数

--- control/api/GoodsController.php
+++ control/api/GoodsController.php
@@ -4,6 +4,9 @@ require(DOC_ROOT. '/control/BaseController.php');
 class GoodsController extends BaseController {
     //商品列表
     public function listAction() {
-        $this->renderJSON(Config::getOptions('goods'));  
+        $goods = Config::getOptions('goods');
+        $encryped = Helper::encodeParams($goods); 
+        $decryped = Helper::decodeParams($encryped);
+        $this->renderJSON($goods);  
     }
 }
