f03cf9f813c1ba9bca247e5ee9d8030194fd43e2
joy <joy.cood@gmail.com>
2017-06-14 14:51:41 +0800

    添加缺失文件

---
+++ control/api/SystemController.php
@@ -0,0 +1,32 @@
+<?php
+require(DOC_ROOT. '/control/BaseController.php');
+
+class SystemController extends BaseController {
+    //一些开关设置
+    public function toggleAction() {
+        $data = array(
+            'broadcast'   => Config::BROADCAST_ENABLED,
+            'phone-login' => Config::PHONE_LOGIN_ENABLED,
+            'phone-reg'   => Config::PHONE_REG_ENABLED,
+            'apple-pay'   => Config::APPLE_PAY_ENABLED
+        );
+        $this->responseJSON($data);
+    }
+
+    //系统公告跑马灯
+    public function broadcastAction() {
+	    $filters = array();
+		$projection = array('_id'=>0, 'Content'=>1);
+        $cursor = Admin::model('notice.main')->find($filters, $projection);
+		$data = array();
+		foreach($cursor as $item) {
+		    $data[] = $item['Content'];
+		}
+		$data = array(
+			'亲们，如发现游戏bug或建议请联系微信号：70612568',
+			'拉上你的好友一起来玩吧'
+		);
+		$this->responseJSON($data);
+
+	}    
+}
