d5a4bc7578cb092e17c99d7aa84e252c8103d611
guriqi <guriqi@long.tv>
2017-06-15 07:16:57 +0800

    自定义充值

--- control/region/CardController.php
+++ control/region/CardController.php
@@ -15,14 +15,14 @@ class CardController extends BaseController {
 
 	protected function rechargeForm() {
 		$this->render('card/recharge.html', array(
-			'options' => require(DOC_ROOT. '/conf/card.config.php')
+			'options' => Config::getOptions('card')
 		));
 	}
 
 	protected function recharge() {
 		$target   = intval($this->request->post('target'));
 	    $optValue = trim($this->request->post('option'));
-		$options  = require(DOC_ROOT. '/conf/card.config.php');
+		$options  = Config::getOptions('card');
 	    $quantity = 0;
 	    $money    = 0;
 
@@ -44,6 +44,61 @@ class CardController extends BaseController {
 		}
 		if($quantity<0) {
 		    $this->error('请输入正整数');
+		}
+		if($trader['RoomCard']<$quantity) {
+		    $this->error('房卡不足，请联系客服购买房卡');
+		}
+		//先给房家充值成功后再扣除代理商的房卡
+		$filters = array('_id' => strval($target));
+		$update  = array('$inc'=>array('RoomCard'=>$quantity));
+		$result  = $User->findAndModify($filters, $update);
+		if($result) { //扣除代理商的对应数量的房卡
+			$quantity *= -1;
+			$filters = array('_id' => $_SESSION[Config::SESSION_UID]);
+			$update = array('$inc'=>array('RoomCard'=>$quantity));
+			$options = array('new' => true);
+			$User->findAndModify($filters, $update, null, $options);
+		}
+
+		$data = $this->getUserInfo($target);
+		$this->renderJSON($data? $data: array());
+	}
+
+	public function customRechargeAction() {
+		if($this->request->isGet()) {
+			$this->setCsrfToken();
+			$this->customRechargeForm();
+		} else if($this->request->isPost()) {
+			$this->checkCsrfToken();
+			$this->customRecharge();
+		}
+	}
+
+	protected function customRechargeForm() {
+		$this->render('card/custom-recharge.html', array(
+			'options' => Config::getOptions('card')
+		));
+	}
+
+	protected function customRecharge() {
+		$target   = intval($this->request->post('target'));
+	    $quantity = intval($this->request->post('quantity'));
+	    $money    = floatval($this->request->post('money'));
+
+		$User    = Admin::model('user.main');
+		$filters = array('_id' => $_SESSION[Config::SESSION_UID]);
+		$trader  = $User->findOne($filters);
+		if(!$trader) {
+		    $this->error('请重新登录');
+		}
+		if(!$target) {
+		    $this->error('请输入玩家游戏ID');
+		}
+		if($quantity<0) {
+		    $this->error('房卡请输入正整数');
+		}
+		if($money<0) {
+		    $this->error('请输金额不能小于0');
 		}
 		if($trader['RoomCard']<$quantity) {
 		    $this->error('房卡不足，请联系客服购买房卡');
