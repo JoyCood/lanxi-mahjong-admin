c909d0a232d52731065664028af8ddcd343cf325
joy <joy.cood@gmail.com>
2017-07-07 15:47:04 +0800

    添加缺失版本配置文件

---
+++ conf/release.config.php
@@ -0,0 +1,13 @@
+<?php 
+return array(
+    'android' => array(
+        'version'  => '2.2.0',
+        'download' => 'http://cdn.yiiyu.cn/apk/lxqs_v1.0.0.apk', 
+        'summary'  => '版本更新描述'
+    ),
+    'ios' => array(
+        'version'  => '1.0.0',
+        'download' => 'https://fir.im/LXQS',
+        'summary'  => '版本更新描述'
+    ) 
+);
